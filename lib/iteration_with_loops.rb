cb4c3d2d6dd32b5c51b8bf210ee054d872d798b8
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-16 22:45:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,7 @@
 def join_nested_strings(src)
+  row_index = 0 
+  final_string = ""
+  
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
 end
